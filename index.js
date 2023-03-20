97b033ef8e1aa748bd37fe84eed7de90655bc891
charlie-s1 <charlie.d.smith98@gmail.com>
2023-03-20 13:16:11 +0000

    add better menu

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 window.addEventListener("load",()=>{
     const cards = document.querySelectorAll('.card');
 
-    function rotateToMouse(e) {      
-        e.target.parentNode.style.transform = `
-          scale3d(1.07, 1.07, 1.07)
-        `;  
-    }
+   
     for(c of cards){
-        c.addEventListener("mousemove",rotateToMouse);
         c.addEventListener("mouseenter",(e)=>{
             e.target.querySelector("video") ? e.target.querySelector("video").play() : null;
             e.target.querySelector(".titleCont").style.top="50%";
+            e.target.parentNode.style.transform = `
+                scale3d(1.07, 1.07, 1.07)
+            `; 
             e.target.style.zIndex = "2";
         })
         c.addEventListener("mouseleave",(e)=>{
             e.target.querySelector("video") ? e.target.querySelector("video").pause() : null;
-            e.target.style.transform="";
+            e.target.parentNode.style.transform="";
             e.target.style.zIndex = "1";
-            e.target.querySelector(".cardHover").style.background="";
+            // e.target.querySelector(".cardHover").style.background="";
             e.target.querySelector(".titleCont").style.top="85%";
         })
 
--- main.js
+++ main.js
@@ -3,7 +3,8 @@ window.addEventListener("load",()=>{
     document.querySelector("#closeNav").addEventListener("click",toggleMenu);
 });
 function toggleMenu(e){
-    const nav = document.querySelector(".navCont");
+    const nav = document.querySelector("#mobMenu");
     nav.classList.toggle("showNav");
     document.body.classList.toggle("stopScroll");
+    // document.querySelector(".navBackground").classList.toggle("showNavBackground");
 }
