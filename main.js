ff3b5eec3856ecbf386c27254da34b92a5b90971
charlie-s1 <charlie.d.smith98@gmail.com>
2023-03-27 12:26:43 +0100

    animate menu toggle

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 window.addEventListener("load",()=>{
     document.querySelector("#menuIcon").addEventListener("click",toggleMenu);
-    document.querySelector("#closeNav").addEventListener("click",toggleMenu);
 });
 function toggleMenu(e){
+    const menuIconTop = document.querySelector("#topLine");
+    const menuIconMid = document.querySelector("#midLine");
+    const menuIconBottom = document.querySelector("#bottomLine");
+    const menuIcon = document.querySelector("#menuIcon");
+
     const nav = document.querySelector("#mobMenu");
     nav.classList.toggle("showNav");
     document.body.classList.toggle("stopScroll");
-    // document.querySelector(".navBackground").classList.toggle("showNavBackground");
+
+    // !menuIcon.style["justify-content"] ? menuIcon.style["justify-content"]="center" : menuIcon.style["justify-content"]=null;
+    !menuIconTop.style.cssText ? menuIconTop.style.cssText = 
+    `
+        transform:rotate(45deg);
+        top:29%;
+    `
+    : menuIconTop.style.cssText = 
+    `
+        transform:null;
+        top:null;
+    `;
+    !menuIconBottom.style.cssText ? menuIconBottom.style.cssText = 
+    `
+        transform:rotate(-45deg);
+        top:-29%;
+    `: menuIconBottom.style.cssText = null;
+    !menuIconMid.style.width ? menuIconMid.style.width = "0": menuIconMid.style.width = null;
 }
