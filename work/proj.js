1206269fd60d7220ca5005a8a983a9099b30154e
charlie-s1 <charlie.d.smith98@gmail.com>
2023-05-09 14:30:15 +0100

    move main page to work

--- work/proj.js
+++ work/proj.js
@@ -2,12 +2,14 @@ window.onload = ()=>{
     const imgContainers = document.querySelectorAll(".projCont");
     for(const c of imgContainers){
         c.addEventListener("mouseenter",()=>{
-            // c.querySelector(".imageDesc").style.display="block";
             c.querySelector(".imageDesc").style.opacity=1;
+            c.querySelector(".imageDesc").style.zIndex=10;
         });
         c.addEventListener("mouseleave",()=>{
-            // c.querySelector(".imageDesc").style.display="none";
             c.querySelector(".imageDesc").style.opacity=0;
+            c.querySelector(".imageDesc").style.zIndex=-1;
         });
+        c.addEventListener("click",()=>{
+        })
     }
 }
