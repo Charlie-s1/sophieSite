54fdbad57103c1aa6df276572af49558006c4fac
charlie-s1 <charlie.d.smith98@gmail.com>
2023-05-29 17:48:35 +0100

    add new homepage

--- home.js
+++ home.js
@@ -7,11 +7,11 @@ function mobView(){
   const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
   if(width<=800){
     console.log("mob")
-    document.querySelector("#desktop").style.display = "none";
+    document.querySelector("#desktop2").style.display = "none";
     document.querySelector("#mob").style.display = "flex";
   }else{
     console.log("desk")
-    document.querySelector("#desktop").style.display = "flex";
+    document.querySelector("#desktop2").style.display = "flex";
     document.querySelector("#mob").style.display = "none";
   }
 }
